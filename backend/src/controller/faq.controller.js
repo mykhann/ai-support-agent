15819b1a2e35784a9e198204e09449f413f88765
yousuf khann <mykhann10@gmail.com>
2026-06-07 01:03:57 +0500

    fix (controller): move validation outside condition block

--- backend/src/controller/faq.controller.js
+++ backend/src/controller/faq.controller.js
@@ -4,26 +4,29 @@ export const addFaq=async(req,res)=>{
    try {
      const {question,answer}=req.body;
     if (!question || !answer){
-        res.status(400).json({
+       return res.status(400).json({
             success:false,
             message:"question and answer are required"
-        })
+        })}
 
     const addFAQ=await FAQ.create({
         question,
         answer
     })    
 
-    res.status(200).json({
+   return res.status(200).json({
         success:true,
         message:"faq added successfully",
         data:addFaq
     })
 
-    }
+    
     
    } catch (error) {
-    res.status(500).json(error.message)
+    res.status(500).json({
+        success:false,
+        message:error.message
+    })
     
    }
 }

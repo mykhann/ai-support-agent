9f83a1aeb47a95e6142b5c50226eb44019a8b7b8
yousuf khann <mykhann10@gmail.com>
2026-06-06 23:59:14 +0500

    feat (server): use db

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import {connectDB} from "./src/config/db.js"
 
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+// DB connection 
+connectDB()
+
 
 
 

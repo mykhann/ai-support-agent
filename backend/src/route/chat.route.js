b5e4cac250178bbf350462235cb0428a6bd2579f
yousuf khann <mykhann10@gmail.com>
2026-06-07 01:48:06 +0500

    feat (route) : add get chat history

--- backend/src/route/chat.route.js
+++ backend/src/route/chat.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { chatWithBot } from "../controller/chat.controller.js";
+import { chatWithBot, getChatHistory } from "../controller/chat.controller.js";
 import { chatLimiter } from "../middleware/rateLimit.middleware.js";
 
 const router=Router()
 
 router.post("/",chatLimiter,chatWithBot)
+router.get("/:sessionId", getChatHistory);
 
 
 export default router;
